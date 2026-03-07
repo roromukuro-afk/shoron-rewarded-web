843193df611641c571f57a6490bd7bba2452402e
roromukuro-afk <roromukuro-afk@users.noreply.github.com>
2026-03-07 17:01:18 +0900

    add robots file

---
+++ app/robots.ts
@@ -0,0 +1,11 @@
+import type { MetadataRoute } from "next";
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: "*",
+      allow: "/",
+    },
+    sitemap: "https://shoron-rewarded-web.vercel.app/sitemap.xml",
+  };
+}
